a8fb738fc82e16bf614eeaea20c9c937c4f65059
brajpatel <brajeshpatel0002@gmail.com>
2023-04-24 09:45:27 +0100

    Import card images

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import Card from "./Card";
+import angular from "../assets/angular.png";
+import cPlusPlus from "../assets/c-plus-plus.png";
+import css from "../assets/css.png";
+import github from "../assets/github.png";
+import html from "../assets/html.png";
+import java from "../assets/java.png";
+import javascript from "../assets/javascript.png";
+import mongodb from "../assets/mongodb.png";
+import nodeJs from "../assets/node-js.png";
+import python from "../assets/python.png";
+import reactJs from "../assets/react.png";
+import ruby from "../assets/ruby.png";
 
 function Game() {
     return (
