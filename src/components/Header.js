02727e8323b433f748888e1463087301cac75b4d
brajpatel <brajeshpatel0002@gmail.com>
2023-04-24 09:29:57 +0100

    Create simple header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Header() {
-
+    return (
+        <header>
+            <h1>Memory Card Game</h1>
+            <div className="scores">
+                <p>Score: 0</p>
+                <p>High Score</p>
+            </div>
+        </header>
+    )
 }
 
 export default Header;
